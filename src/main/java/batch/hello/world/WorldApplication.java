51b457fa30e07987c93bde6beab08bc820bc145b
jingug1004 <jingug1004@gmail.com>
2023-03-20 18:52:37 +0900

    feat: 첫번째 배치 로직
    앞으로가 더욱 기대된다

---
+++ src/main/java/batch/hello/world/WorldApplication.java
@@ -0,0 +1,44 @@
+package batch.hello.world;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class WorldApplication {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Step step() {
+        return this.stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Hello, World!");
+
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Job job() {
+        return this.jobBuilderFactory.get("job")
+                .start(step()).build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldApplication.class, args);
+    }
+
+}
