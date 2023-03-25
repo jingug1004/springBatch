f4183dda59769919ec194edc0ddb6d91501b479a
jingug1004 <jingug1004@gmail.com>
2023-03-25 21:10:32 +0900

    feat: 다섯번째 배치 step 중에 tasklet들

---
+++ src/main/java/batch/hello/world/tasklets/CustomService.java
@@ -0,0 +1,14 @@
+package batch.hello.world.tasklets;
+
+public class CustomService {
+
+//    private String param;
+//
+//    public CustomService(String param) {
+//        this.param = param;
+//    }
+
+    public void serviceMethod(String message) {
+        System.out.println("l~ Service method was called -> " + message);
+    }
+}
