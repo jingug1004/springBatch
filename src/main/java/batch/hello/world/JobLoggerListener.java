be45272c04f7d54a34cff5476a4cebbc5e44bd70
jingug1004 <jingug1004@gmail.com>
2023-03-25 16:56:22 +0900

    feat: 세번째 배치 로직
    basicJob

---
+++ src/main/java/batch/hello/world/JobLoggerListener.java
@@ -0,0 +1,25 @@
+package batch.hello.world;
+
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+
+public class JobLoggerListener implements JobExecutionListener {
+
+    private static String START_MESSAGE = "%s is beginning execution";
+    private static String END_MESSAGE = "%s has completed with the status %s";
+
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+        System.out.println("☆☆☆☆☆☆ l~ " + String.format(START_MESSAGE,
+                jobExecution.getJobInstance().getJobName()
+        ));
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        System.out.println("★★★★★★ l~ " + String.format(END_MESSAGE,
+                jobExecution.getJobInstance().getJobName(),
+                jobExecution.getStatus()
+        ));
+    }
+}
--- src/main/java/batch/hello/world/WorldApplication.java
+++ src/main/java/batch/hello/world/WorldApplication.java
@@ -5,11 +5,19 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @EnableBatchProcessing
 @SpringBootApplication
@@ -22,20 +30,64 @@ public class WorldApplication {
     private StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Step step1() {
-        return this.stepBuilderFactory.get("step1")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("Hello, World!");
+    public CompositeJobParametersValidator validator() {
+        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
 
-                    return RepeatStatus.FINISHED;
-                }).build();
+        DefaultJobParametersValidator defaultJobParametersValidator =
+                new DefaultJobParametersValidator(
+                        new String[]{"fileName"},
+                        new String[]{"name", "run.id"}
+                );
+        defaultJobParametersValidator.afterPropertiesSet();
+
+        validator.setValidators(
+                Arrays.asList(new DefaultJobParametersValidator(), defaultJobParametersValidator));
+
+        return validator;
     }
 
     @Bean
     public Job job() {
         return this.jobBuilderFactory.get("basicJob")
-                .start(step1()).build();
+                .start(step1())
+                .validator(validator())
+                .incrementer(new RunIdIncrementer())
+                .listener(new JobLoggerListener())
+                .build();
     }
+
+    @Bean
+    public Step step1() {
+        return this.stepBuilderFactory.get("step1")
+                .tasklet(helloWorldTasklet(null, null))
+                .build();
+    }
+
+//    @Bean
+//    public Tasklet helloWorldTasklet() {
+//
+//        return (contribution, chunkContext) -> {
+//            String name = (String) chunkContext.getStepContext()
+//                    .getJobParameters()
+//                    .get("name");
+//
+//            System.out.println(String.format("l~l~l~l~l~l~ hello, %s!", name));
+//            return RepeatStatus.FINISHED;
+//        };
+//    }
+
+    @StepScope
+    @Bean
+    public Tasklet helloWorldTasklet(
+            @Value("#{jobParameters['name']}") String name,
+            @Value("#{jobParameters['fileName']}") String fileName) {
+        return (contribution, chunckContext) -> {
+            System.out.println(String.format("l~ hello, %s!", name));
+            System.out.println(String.format("l~l~ fileName = %s!", fileName));
+            return RepeatStatus.FINISHED;
+        };
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(WorldApplication.class, args);
